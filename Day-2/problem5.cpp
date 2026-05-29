1857949b89f94e5a8e6edbd40e7dfaad566f7964
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-05-29 21:24:54 +0530

    Add digit sum calculator in problem5.cpp
    Implement a program to calculate the sum of digits of a number.

---
+++ Day-2/problem5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number :";
+    cin>>n;
+    int s=0;
+    while (n>0){
+        s=s+(n%10);
+        n=n/10;
+    }
+    cout<<"The sum of digits of your number is : "<< s;
+    return 0;
+}
