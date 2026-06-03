8ee339fc8780200ea2ea86eb60e6d95a579c7d39
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-06-03 09:34:53 +0530

    Add prime factorization program in C++

---
+++ Day-5/Assignment-3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void primeFactors(int n) {
+    while (n % 2 == 0) {
+        cout << 2 << " ";
+        n /= 2;
+    }
+
+    for (int i=3; i<=sqrt(n); i+=2) {
+        while (n % i == 0) {
+            cout << i << " ";
+            n /= i;
+        }
+    }
+
+    if (n > 2)
+        cout << n << " ";
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Prime factors of " << n << " are: ";
+    primeFactors(n);
+    return 0;
+}
