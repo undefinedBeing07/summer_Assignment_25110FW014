00a42875375e6a8250dea8fc0048145e8fbb5e2c
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-06-07 22:57:42 +0530

    Add pyramid pattern printing program in C++
    Implement a program to print a pyramid pattern using characters based on user input for the number of rows.

---
+++ Day-10/Assignment-4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter number of rows:";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<char(j+64);
+        }
+        if(i>1){
+            for(int j=i-1;j>0;j--){
+                cout<<char(j+64);
+            }
+
+        }
+        
+        cout<<endl;
+    }
+}
