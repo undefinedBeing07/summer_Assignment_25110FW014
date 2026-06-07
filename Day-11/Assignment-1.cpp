3df462af50d3d7b8552940a054387435187b151f
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-06-07 23:00:15 +0530

    Add C++ program to calculate sum of two numbers

---
+++ Day-11/Assignment-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int sum(int a,int b){
+    return a+b;
+}
+int main() {
+    int a,b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    cout<<"Sum is: "<<sum(a,b);
+}
