a29573d965f5eced798e8ec5e247e74811a06da3
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-06-16 09:29:19 +0530

    Add intersection logic for two integer arrays
    Implement intersection of two arrays using a set.

---
+++ Day-17/Assignment-3.cpp
@@ -0,0 +1,22 @@
+// union of intersection
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> arr1={1,3,4,5,7,9};
+    vector<int> arr2 = {1,2,4,6,8};
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+    
+    set<int> s;
+    for(int i=0;i<n1;i++){
+        s.insert(arr1[i]);
+    }
+    
+    //intersection condition
+    for(int i=0;i<n2;i++){
+        if(s.find(arr2[i])!=s.end()){
+            cout<<arr2[i]<<" ";
+        }
+    }
+    
+}
