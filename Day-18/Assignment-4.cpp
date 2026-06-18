dba5caa480770aa90d6264b745e4a9af2800e8d0
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-06-18 12:03:12 +0530

    Add bubble sort implementation in Assignment-4.cpp
    Implement bubble sort algorithm to sort an array.

---
+++ Day-18/Assignment-4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <algorithm>
+
+using namespace std;
+int main(){
+    int arr[]={1,2,3,5,4};
+    int size=sizeof(arr)/4;
+
+    for(int i=0;i<size-1;i++){
+
+        bool flag=true;
+
+        for(int j=0;j<size-1-i;j++){
+
+            if(arr[j]<arr[j+1]){
+
+                swap(arr[j],arr[j+1]);
+
+                flag=false;
+            }
+        }
+        if(flag) break;
+        
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
