fee8059acf5aabca0d6dfcb4380c0d9d5175cee6
undefinedBeing07 <shauryaraghuvanshi04@gmail.com>
2026-06-26 12:26:21 +0530

    Add age eligibility check for voting

---
+++ Day-26/Assignment-2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter the age to check for the eligibility: ";
+    cin>>age;
+    
+    if(age<18) cout<<"You are not eligible!!!"<<"\n";
+    else cout<<"You are eligible to vote!!"<<"\n";
+}
